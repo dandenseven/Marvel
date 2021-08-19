580a9b4766a7e66a585a1bde92a6b2456c3d1c9e
dknight <knight1seven@gmail.com>
2021-08-18 22:12:58 -0400

    environment variable

--- marvellous.py
+++ marvellous.py
@@ -11,7 +11,7 @@ API_PRIVATE = config('PRIVATE')
 API_HASH = config('HASH')
 
 response = requests.get(
-    'http://gateway.marvel.com/v1/public/characters?ts=1629289209&apikey=API_PUBLIC&hash=API_HASH')
+    f'http://gateway.marvel.com/v1/public/characters?ts=1629289209&apikey={API_PUBLIC}&hash={API_HASH}')
 
 if response.status_code == 200:
     print("The request was a success!")
